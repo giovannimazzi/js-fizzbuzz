84049d3e1a4ebcb9e751a73aa5ec75d7166076b6
giovannimazzi <gioelor@libero.it>
2025-12-17 15:52:57 +0100

    inserito il filtro per intercettare i multipli di 3 e 5 e stampare Fizz, Buzz, o FizzBuzz

--- main.js
+++ main.js
@@ -3,8 +3,24 @@ let output;
 
 //Ciclo che scorre i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-  //Assegno a variabile di output il valore del contatore
-  output = i;
+  //Inizializzo variabile di output con stringa vuota
+  output = "";
+
+  //Intercetto multipli di 3
+  if (i % 3 === 0) {
+    output += "Fizz";
+  }
+
+  //Intercetto multipli di 5
+  if (i % 5 === 0) {
+    output += "Buzz";
+  }
+
+  /*Se la variabile di output è ancora stringa vuota, 
+    le assegno il valore corrente del contatore*/
+  if (!output) {
+    output = i;
+  }
 
   //Stampo il valore della variabile di output
   console.log(output);
