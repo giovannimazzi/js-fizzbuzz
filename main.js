a2add7fdd05ed34a625d9122678796b00bc3d3de
giovannimazzi <gioelor@libero.it>
2025-12-17 15:51:57 +0100

    stampa tutti i numeri da 1 a 100

--- main.js
+++ main.js
@@ -2,4 +2,10 @@
 let output;
 
 //Ciclo che scorre i numeri da 1 a 100
-for (let i = 1; i <= 100; i++) {}
+for (let i = 1; i <= 100; i++) {
+  //Assegno a variabile di output il valore del contatore
+  output = i;
+
+  //Stampo il valore della variabile di output
+  console.log(output);
+}
