c8fbdf330f3d9366f31450247810f804fdd4bc10
giovannimazzi <gioelor@libero.it>
2025-12-17 16:30:06 +0100

    correzione sulla versione di Stefano, e inizio versione di Tiziano

--- main.js
+++ main.js
@@ -33,11 +33,11 @@ for (let i = 1; i <= 100; i++) {
 
 // ? Non usare la concatenazione, ma stampa diretta
 
-//Variabile per mostrare l'output
+/* //Variabile per mostrare l'output
 let output;
 
 //Variabili per individuare multipli
-let isMulti3, isMulti5, isMulti3and5;
+let isMulti3, isMulti5;
 
 //Ciclo che scorre i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
@@ -55,10 +55,7 @@ for (let i = 1; i <= 100; i++) {
     isMulti5 = true;
   }
 
-  //Valuto se è multiplo di entrambi
-  isMulti3and5 = isMulti3 && isMulti5;
-
-  if (isMulti3and5) {
+  if (isMulti3 && isMulti5) {
     output = "FizzBuzz";
   } else if (isMulti3) {
     output = "Fizz";
@@ -70,5 +67,45 @@ for (let i = 1; i <= 100; i++) {
 
   //Stampo il valore della variabile di output
   console.log(output);
-}
+} */
 //***************************************************** */
+
+// * VERSIONE BONUS TIZIANO
+
+// ? fai scegliere all'utente un numero 
+// ? custom di cui sostituire tutti i multipli con la parola "Boom"
+
+//Variabile per mostrare l'output
+let output;
+
+//Chiedo all'utente numero custom
+const 
+
+//Ciclo che scorre i numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+  //Inizializzo variabile di output con stringa vuota
+  output = "";
+
+  //Intercetto multipli di 3
+  if (i % 3 === 0) {
+    output += "Fizz";
+  }
+
+  //Intercetto multipli di 5
+  if (i % 5 === 0) {
+    output += "Buzz";
+  }
+
+  //Se la variabile di output è ancora stringa vuota, 
+  //le assegno il valore corrente del contatore
+  if (!output) {
+    output = i;
+  }
+
+  //Stampo il valore della variabile di output
+  console.log(output);
+}
+
+//***************************************************** */
+
+
